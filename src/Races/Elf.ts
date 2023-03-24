69b4bc8585b5516793ceb6ff8e1bd3976d448b91
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-24 17:39:52 -0300

    Feat: Implementa a classe herdeira de Race Halfling

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -6,7 +6,7 @@ export default class Elf extends Race {
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
-    this._maxLifePoints = 80;
+    this._maxLifePoints = 99;
     Elf.count += 1;
   }
 
