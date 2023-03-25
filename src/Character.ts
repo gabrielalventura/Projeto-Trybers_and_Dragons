7cb90cec5d280d082a4a6f1d15cd43cf75805158
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-25 10:29:34 -0300

    Feat: Define algumas propriedades de levelUp da interface Fighter

--- src/Character.ts
+++ src/Character.ts
@@ -74,4 +74,15 @@ export default class Character implements Fighter {
   attack(enemy: Fighter): void {
     enemy.receiveDamage(this._strength);
   }
+
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10); 
+    this._energy.amount = 10; // chamar somente a chave necessária no objeto;
+  }
 }
