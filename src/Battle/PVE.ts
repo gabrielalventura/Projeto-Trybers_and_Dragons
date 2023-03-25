fa93eae68cbdbd34e8abb7590da9934a63286190
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-25 19:23:17 -0300

    Fix: Refatora as interfaces Fighter e SimpleFighter para utilização das mesmas no array de _environment

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -3,11 +3,23 @@ import Battle from './Battle';
 
 export default class PVE extends Battle {
   private _character: Fighter;
-  private _environment: SimpleFighter[] | Fighter[];
+  private _environment: Fighter[] | SimpleFighter[];
 
-  constructor(character: Fighter, environment: SimpleFighter[] | Fighter[]) {
+  constructor(character: Fighter, environment: Fighter[] | SimpleFighter[]) {
     super(character);
     this._character = character;
     this._environment = environment;
+  }
+
+  fight(): number {
+    this._environment.forEach((e) => {
+      do {
+        this._character.attack(e);
+        e.attack(this._character);
+      } while (this._character.lifePoints > 0 && e.lifePoints > 0);
+      this._character.attack(e);
+      e.attack(this._character);
+    });
+    return this._character.lifePoints === -1 ? -1 : 1;
   }
 }
