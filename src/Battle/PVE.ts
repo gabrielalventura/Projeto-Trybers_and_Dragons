9e72c4802202aa800ff00b5969d6cdbdb003b7e7
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-25 19:04:27 -0300

    Feat: Define atributos da classe PVE

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,5 +1,13 @@
+import Fighter, { SimpleFighter } from '../Fighter';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
-  
+  private _character: Fighter;
+  private _environment: SimpleFighter[] | Fighter[];
+
+  constructor(character: Fighter, environment: SimpleFighter[] | Fighter[]) {
+    super(character);
+    this._character = character;
+    this._environment = environment;
+  }
 }
