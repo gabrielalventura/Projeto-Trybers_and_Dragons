00b75fef551108d21c1dc2560115d6e92d5238f8
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-27 14:49:44 -0300

    Feat: exporta monsters

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
 
 const player1 = new Character('player1');
 const player2 = new Character('player2');
@@ -9,8 +11,13 @@ player1.levelUp();
 player1.levelUp();
 player1.levelUp();
 
+const monster1 = new Monster('monster1');
+const monster2 = new Dragon('monster2');
+
 export {
   player1,
   player2,
   player3,
+  monster1,
+  monster2,
 };
