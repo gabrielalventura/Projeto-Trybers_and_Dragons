7dc75a140bfb1a7b3ece8f2d1e8126439725c3f1
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-27 14:32:19 -0300

    Feat: Salva todas as adequações e alterações de refatorações

--- src/Dragon.ts
+++ src/Dragon.ts
@@ -6,3 +6,4 @@ export default class Dragon extends Monster {
     this._lifePoints = 999;
   }
 }
+// uma vez que agora _lifePoints é protected na class Monster, é possivel para uma classe herdeira ler e até escrever no atributo, mas não é permitido que classes/situações de fora da classe Monster e suas herdeiras faça o mesmo.
